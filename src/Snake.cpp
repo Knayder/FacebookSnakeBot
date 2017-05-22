1fcd065d3d9121c813053ac0e55594b7306ecf23
Knayder <knayder@outlook.com>
2017-05-22 22:53:22 +0200

    Getting ready to make separated class for snake detection

---
+++ src/Snake.cpp
@@ -0,0 +1,9 @@
+#include "Snake.h"
+
+Snake::Snake(const sf::Vector2f & tileSize, const sf::Vector2i & tilesAmount, const sf::Vector2i & size) :
+	tileSize(tileSize),
+	tilesAmount(tilesAmount),
+	size(size)
+{
+
+}
--- src/Snake.h
+++ src/Snake.h
@@ -1,9 +1,12 @@
 #pragma once
-
+#include <SFML\Graphics.hpp>
 
 class Snake {
 public:
-	Snake();
+	Snake(const sf::Vector2f &tileSize, const sf::Vector2i &tilesAmount, const sf::Vector2i &size);
 
 private:
+	sf::Vector2f tileSize;
+	sf::Vector2i tilesAmount;
+	sf::Vector2i size;
 };
